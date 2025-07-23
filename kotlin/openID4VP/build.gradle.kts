a03d1bbe0234c70f088b49db92fb751e33a479fb
Alka <prasadalka1998@gmail.com>
2025-07-23 22:46:42 +0530

    [INJIMOB-3388]: remove dummy test pushed
    Signed-off-by: Alka <prasadalka1998@gmail.com>

--- kotlin/openID4VP/build.gradle.kts
+++ kotlin/openID4VP/build.gradle.kts
@@ -48,7 +48,6 @@ kotlin {
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
-                //implementation(kotlin("test-junit"))
                 implementation(libs.junit)
                 implementation(libs.mockk)
                 implementation(libs.mockwebserver)
