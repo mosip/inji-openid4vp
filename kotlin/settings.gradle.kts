22aeaecc6771fee55ab131539471ff9756c3b1da
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2025-08-22 15:27:21 +0530

    [INJIMOB-3408] refactor: rm unused repository link
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- kotlin/settings.gradle.kts
+++ kotlin/settings.gradle.kts
@@ -18,7 +18,6 @@ dependencyResolutionManagement {
         mavenCentral()
         maven { url = uri("https://repo.danubetech.com/repository/maven-public/") }
         maven { url = uri("https://jitpack.io") }
-        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
         maven { url = uri("https://central.sonatype.com/repository/maven-snapshots/") }
     }
 }
